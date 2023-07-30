0c05b5af31e0a02a370f7dfe63d2bcd4ae91ba25
超哥呦 <boy_0214@sina.com>
2023-07-30 17:27:47 +0800

    重构数据源管理页面

--- src/main/java/com/itboy/controller/DataSourceManagerController.java
+++ src/main/java/com/itboy/controller/DataSourceManagerController.java
@@ -33,8 +33,8 @@ public class DataSourceManagerController {
 
 
     @RequestMapping("/page")
-    public String dbsourcePage() {
-        return "dbsourcePage";
+    public String dataSourcePage() {
+        return "dataSourcePage";
     }
 
 
